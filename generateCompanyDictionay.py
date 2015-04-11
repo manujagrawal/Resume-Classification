2477c9bf998ee301f8b8cca6c1da287491ec9229
manujagrawal <ma.ag37@gmail.com>
2015-04-11 07:39:56 +0530

    company label extractor added

--- generateCompanyDictionay.py
+++ generateCompanyDictionay.py
@@ -1,1 +1,20 @@
+import numpy as np
+import csv
+
+f = open('company_labels.csv', 'r')
+reader = csv.reader(f)
+
+companies = []
+labels = []
+
+for row in reader:
+	companies.append(row[0])
+	row2=[float(i) for i in row[1:] ]
+	labels.append(row2)
+
+companies=np.array(companies)
+labels=np.array(labels)
+
+print companies.shape  
+print labels.shape 
 
